0f5e94b091df188e38d398eb97156e1c3efbbc34
Zen-03 <aldaydenrick41@gmail.com>
2026-05-23 23:03:58 +0800

    initial

--- server/index.js
+++ server/index.js
@@ -53,7 +53,7 @@ app.use(cors({
         'http://127.0.0.1:5173',
         'http://127.0.0.1:4173',
         'https://eguide-deployment.onrender.com',
-        'https://eguide-deployment.vercel.app/'
+        'https://eguide-deployment.vercel.app'
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
